a62e2e80b503b2d8a947b16973b07952112bf662
Kenneth Waters <kwwaters@gmail.com>
2010-08-24 13:47:26 -0700

    Switch to using WebGL from Canvas2D.

--- halo.js
+++ halo.js
@@ -1,21 +1,41 @@
 
-
-var twod;
+var gl;
 
 (function() {
 
-var resize = function(twod) {
-    twod.setTransform(1., 0., 0., 1., 0., 0.);
-    twod.scale(256., -256.)
-    twod.translate(1., -1.);
+// var gl;
+
+var resize = function() {
+}
+
+var loadProgram = function(vtx_id, frag_id) {
+    // Cleanup.
+    var program = gl.createProgram();
+
+    var vertex = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(vertex, document.getElementById(vtx_id).text);
+    gl.compileShader(vertex);
+    console.log('vertex:\n' + gl.getShaderInfoLog(vertex));
+    gl.attachShader(program, vertex);
+    var fragment = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(fragment, document.getElementById(frag_id).text);
+    gl.compileShader(fragment);
+    console.log('fragment:\n' + gl.getShaderInfoLog(fragment));
+    gl.attachShader(program, fragment);
+    gl.linkProgram(program);
+    console.log('program:\n' + gl.getProgramInfoLog(program));
+
+    gl.useProgram(program);
+
+    return program;
 }
 
 var main = function() {
-    twod = $("#c")[0].getContext('2d');
-    resize(twod);
+    gl = $("#c")[0].getContext('webgl');
+    if (!gl)
+      gl = $("#c")[0].getContext('experimental-webgl');
 
-    twod.fillStyle = "rgb(37, 174, 158)";
-    twod.fillRect(-1., -1., 2., 2.);
+    var program = loadProgram("halo_vertex", "halo_fragment");
 
     var points = [
         new Vertex(-.5, .5),
@@ -30,40 +50,57 @@ var main = function() {
 
     var tri = triangulate(points, 1);
 
-    twod.lineCap = "round";
-    twod.lineWidth = .05;
-    twod.beginPath();
+    // TODO: These sizes are a bit conservative.
+    // One vertex per edge + one vertex per control point.
+    var data = new Float32Array(7 * 4 * points.length);
+    var di = 0;
+    // At most one triangle per edge
+    var idx = new Uint16Array(3 * 6 * points.length);
+    var ii = 0;
+
     for (var i = 0, l = points.length; i < l; ++i) {
         var point = points[i];
-        twod.moveTo(point.x, point.y);
-        // WAR, Chrome doesn't correctly handle 0 length lines. Boo.
-        twod.lineTo(point.x + 0.005, point.y);
-    }
-    twod.stroke();
-
-    twod.lineCap = "butt";
-    twod.lineWidth = 0.015;
-
-    twod.strokeStyle = "red";
-    twod.beginPath();
-    for (var i = 0, l = points.length; i < l; ++i) {
-        var vertex = points[i],
-            final_edge = vertex.edge,
-            edge = final_edge,
-            center = edge.face.data.circumcenter();
-        twod.moveTo(center[0], center[1]);
-        edge = edge.twin.next;
-        while (edge !== final_edge) {
+        var anchor = di;
+        var count = 0;
+        var edge = point.edge;
+        do {
+            count += 1;
             var center = edge.face.data.circumcenter();
-            twod.lineTo(center[0], center[1]);
-            edge = edge.twin.next;
-        }
-        twod.closePath();
-        var center = edge.face.data.circumcenter();
-        twod.lineTo(center[0], center[1]);
-    }
-    twod.stroke();
+            data[4 * di + 0] = center[0];
+            data[4 * di + 1] = center[1];
+            data[4 * di + 2] = point.x;
+            data[4 * di + 3] = point.y;
+            ++di;
 
+            edge = edge.twin.next;
+        } while (edge !== point.edge);
+
+        for (j = 1; j < count - 1; ++j) {
+            idx[ii++] = anchor;
+            idx[ii++] = anchor + j;
+            idx[ii++] = anchor + j + 1;
+        }
+    }
+
+    console.log(ii, idx.length);
+    console.log(4 * di, data.length);
+
+    var vbo = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
+    gl.bufferData(gl.ARRAY_BUFFER, data, gl.STREAM_DRAW);
+
+    var ibo = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, ibo);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, idx, gl.STREAM_DRAW);
+
+    var position = gl.getAttribLocation(program, 'position');
+    gl.vertexAttribPointer(position, 2, gl.FLOAT, false, 16, 0);
+    gl.enableVertexAttribArray(position);
+    var control_point = gl.getAttribLocation(program, 'control_point');
+    gl.vertexAttribPointer(control_point, 2, gl.FLOAT, false, 16, 8);
+    gl.enableVertexAttribArray(control_point);
+
+    gl.drawElements(gl.TRIANGLES, ii, gl.UNSIGNED_SHORT, 0);
 }
 
 
